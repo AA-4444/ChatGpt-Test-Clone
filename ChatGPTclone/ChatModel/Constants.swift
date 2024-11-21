3f8a70a1d38e01ce8e8bd75ba2f391d7399f275c
Alex <104495043+AA-4444@users.noreply.github.com>
2024-11-21 18:32:23 +0100

    Add files via upload

---
+++ ChatGPTclone/ChatModel/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatGPTclone
+//
+//  Created by Алексей Зарицький on 16/03/2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let openAIAPIKey = "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"
+}
